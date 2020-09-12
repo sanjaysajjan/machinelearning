45d054cc29472ff70657c599ee82dc9d1adddcb6
sanjaysajjan <71158031+sanjaysajjan@users.noreply.github.com>
2020-09-11 20:13:13 -0400

    Add files via upload
    Write a Python program that implements gradient descent for minimizing
    the least squares loss. As a stopping condition check for the objective (least squares error) between the current and previous iteration. If the objective improves
    by less than theta then you stop. The input and output should be the same
    as for Naive-Bayes. Note that you need to convert label 0 to -1 from training label file since in least squares model  you have outputs of -1 or +1.  But you should display your predictions as 0 instead of -1.
    
    Test your program with the data file testLeastSquares.data and training label file testLeastSquares.trainlabels.0.
    
    Use eta=.001 and stopping condition of .001.
    
    Compute vector [w1, w2] and its distance to origin
    
    Your final w for the test data would be close to
    
    w = [0.087137,0.084443]
    
    and distance of plane to origin would be about
    
    abs(w0/||w||) = 6.754035

---
+++ ss4454_assignment2.py
@@ -0,0 +1,118 @@
+import sys
+import random
+import math
+
+#dot product function
+
+def dot_product(refw, refx, cols):
+    dp=0
+    for j in range(0, cols, 1):
+        dp += refw[j]*refx[j]
+    return dp
+
+datafile = sys.argv[1]
+f = open(datafile)
+#l=line
+data = []
+i = 0
+l = f.readline()
+
+#reading data
+
+while(l != ''):
+    a = l.split()
+    alen = len(a)
+    l2 = []
+    for j in range(0, alen, 1):
+        l2.append(float(a[j]))
+        if j == (alen-1) :
+            l2.append(float(1))
+    data.append(l2)
+    l = f.readline()
+
+rows = len(data)
+cols = len(data[0])
+f.close()
+
+#reading labels
+
+labelfile = sys.argv[2]
+f = open(labelfile)
+class_var = {}
+class_size = []
+class_size.append(0)
+class_size.append(0)
+l = f.readline()
+
+while(l != ''):
+    a = l.split()
+    if int(a[0]) == 0:
+        class_var[int(a[1])] = -1
+    else:
+        class_var[int(a[1])] = int(a[0])
+    l = f.readline()
+    class_size[int(a[0])] += 1
+
+#initializing w
+
+w = []
+for j in range(0, cols, 1):
+    w.append(0)
+    w[j] = (0.02 * random.uniform(0,1)) - 0.01
+
+#Gradient Descent Iteration
+
+eta = 0.0001
+error = 0.0
+
+#computng error
+
+for i in range(0, rows, 1):
+    if (class_var.get(i) != None):
+        error += (-class_var.get(i) + dot_product(w, data[i], cols))**2
+
+temp = 0
+c=0
+
+#computing dellf
+#updating w
+#computing new error
+
+while(temp != 1):
+    c += 1
+    dellf = []
+    for j in range(0,cols,1):
+        dellf.append(0)
+    for i in range(0, rows, 1):
+        if class_var.get(i) != None:
+            dp = dot_product(w, data[i], cols)
+            for j in range(0, cols, 1):
+                dellf[j] += ((-class_var.get(i) + dp)*data[i][j])
+
+    for j in range(0, cols, 1):
+        w[j] = w[j] - eta*dellf[j]
+
+    new_error = 0
+    for i in range(0, rows, 1):
+        if (class_var.get(i) != None):
+            new_error += (-class_var.get(i) + dot_product(w, data[i], cols))**2
+
+    #print ("error is = ",error, c)
+    if (error - new_error) < 0.001:
+        temp = 1
+    error = new_error
+
+#print("count = ",c)
+#print("w = ",w)
+
+#prediction
+
+for i in range(0, rows, 1):
+    if (class_var.get(i) == None):
+        dp = dot_product(w, data[i], cols)
+        #print ("dp is",dp)
+        if(dp>0):
+            print("1 ",i)
+        else:
+            print("0 ",i)
+
